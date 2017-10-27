8136b3794efc15110b32f55350800844e155fe3c
kolotaev <kolotaev@tut.by>
2017-10-27 21:54:22 +0300

    Init

---
+++ src/iron_cache/core.clj
@@ -0,0 +1,6 @@
+(ns iron-cache.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/iron_cache/core_test.clj
@@ -0,0 +1,7 @@
+(ns iron-cache.core-test
+  (:require [clojure.test :refer :all]
+            [iron-cache.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
