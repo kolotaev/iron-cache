6f40b67f5b2c8c7c3b7c4194eef24ff57ab139ed
kolotaev <kolotaev@tut.by>
2017-10-27 23:19:54 +0300

    Making clients

---
+++ src/iron_cache/async.clj
@@ -0,0 +1,1 @@
+(ns iron-cache.async)
--- src/iron_cache/core.clj
+++ src/iron_cache/core.clj
@@ -1,3 +1,17 @@
 (ns iron-cache.core)
 
-(def ^:const AWS_US_EAST_HOST "cache-aws-us-east-1.iron.io")
+(def ^:const ROOT_URL "cache-aws-us-east-1.iron.io")
+
+(def ^:const default_options
+            { :scheme => "https",
+              :host => ROOT_URL,
+              :port => 443,
+              :api_version => 1,
+              :user_agent => "iron_cache_clj"
+              :cache_name => "default"})
+
+(defn new-client
+  [options])
+
+(defn new-async-client
+  [options])
