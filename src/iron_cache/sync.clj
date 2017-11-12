26778d6d82e5d94e5f777822592c5d411e6ebeab
kolotaev <kolotaev@tut.by>
2017-11-12 15:16:04 +0300

    Try to take project and token from ENV

--- src/iron_cache/sync.clj
+++ src/iron_cache/sync.clj
@@ -15,14 +15,21 @@
 (defn new-client
   [options]
   (-> options
-    (merge DEFAULTS options)
+    (merge DEFAULTS options options-from-env)
     validate-options
     ->SyncClient))
+
+
+(defn- options-from-env
+  []
+  {:project (System/getenv "IRON_CACHE_PROJECT")
+   :token (System/getenv "IRON_CACHE_TOKEN")})
+
 
 (defn- validate-options
   [{:keys [project token] :as config}]
   "Validates input options. Throws ex if options are inappropriate for client to work."
   (when-not project
-    (throw (ex-info "A project must be specified." config)))
+    (throw (ex-info "A project must be specified." {:given config})))
   (when-not token
-    (throw (ex-info "An OAuth2 token must be provided." config))))
+    (throw (ex-info "An OAuth2 token must be provided." {:given config}))))
